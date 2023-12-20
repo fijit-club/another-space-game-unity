4ce4b864678794ac38d1cb2ee97c8193cec9b5d4
TusharVaid30 <tusharvaid30@gmail.com>
2023-12-20 17:39:36 +0530

    Minor Changesq

--- Assets/Ability Handler/Scripts/Ability.cs
+++ Assets/Ability Handler/Scripts/Ability.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public abstract class Ability : MonoBehaviour
 {
     [SerializeField] private Animator abilityButton;
-    
-    
+
     public abstract void TriggerAbility();
 
     public abstract void DisableAbility();
