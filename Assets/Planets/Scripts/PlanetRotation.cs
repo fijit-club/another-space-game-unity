02c5edbb3be003b77e63be0c88141e7de3f78244
TusharVaid30 <tusharvaid30@gmail.com>
2024-01-11 17:16:04 +0530

    test

--- Assets/Planets/Scripts/PlanetRotation.cs
+++ Assets/Planets/Scripts/PlanetRotation.cs
@@ -16,16 +16,28 @@ public class PlanetRotation : MonoBehaviour
     [SerializeField]
     public float _rotationSpeed;
 
+    private GameObject _islandSprite;
+    
     private void Start()
     {
         coinRenderer.transform.parent = null;
         collectible.transform.parent = null;
         islandSpriteRenderer.sprite = islandImages[Random.Range(0, islandImages.Length)];
+        _islandSprite = islandSpriteRenderer.gameObject;
     }
 
     private void Update()
     {
-        islandSpriteRenderer.transform.rotation = Quaternion.identity;
+        if (collectible)
+            collectible.transform.eulerAngles = Vector3.zero;
+        if (coinRenderer)
+            coinRenderer.transform.eulerAngles = Vector3.zero;
+        islandSpriteRenderer.transform.parent = null;
         transform.Rotate(0f, 0f, _rotationSpeed * Time.deltaTime * 10f);
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(_islandSprite);
     }
 }
