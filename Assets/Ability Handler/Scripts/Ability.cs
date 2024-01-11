d5c4047565b9479ca37791b5261b7e5b747a88c9
TusharVaid30 <tusharvaid30@gmail.com>
2024-01-11 14:10:11 +0530

    Ability Name

--- Assets/Ability Handler/Scripts/Ability.cs
+++ Assets/Ability Handler/Scripts/Ability.cs
@@ -1,15 +1,21 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public abstract class Ability : MonoBehaviour
 {
     [SerializeField] protected Animator abilityButton;
     [SerializeField] private float abilityTime;
+    [SerializeField] private TMP_Text abilityNameText;
+    [SerializeField] private string abilityName;
     
     protected IEnumerator DisableCounter()
     {
+        abilityNameText.gameObject.SetActive(true);
+        abilityNameText.text = abilityName;
         yield return new WaitForSeconds(abilityTime);
         DisableAbility();
+        abilityNameText.gameObject.SetActive(false);
     }
     
     public abstract void TriggerAbility();
