824085fb98ec75e61209bf035ffb9b0f07fe7d42
TusharVaid30 <tusharvaid30@gmail.com>
2024-02-28 13:27:53 +0530

    slow time lag fix

--- Assets/Ability Handler/Scripts/Abilities/SlowTimeAbility.cs
+++ Assets/Ability Handler/Scripts/Abilities/SlowTimeAbility.cs
@@ -8,11 +8,13 @@ public class SlowTimeAbility : Ability
     public override void TriggerAbility()
     {
         Time.timeScale = reducedTime;
+        Time.fixedDeltaTime = reducedTime * 0.02f;
         StartCoroutine(DisableCounter());
     }
 
     public override void DisableAbility()
     {
         Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f;
     }
 }
